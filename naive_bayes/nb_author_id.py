f678e22c39beb7f6328e9bf0243f4383325c27e0
Mayank Metha D <mayankmethad@gmail.com>
2017-02-04 18:53:15 +0530

    Solution to naive bayes

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -30,4 +30,12 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 
-
+from sklearn.naive_bayes import GaussianNB
+clf = GaussianNB()
+t0 = time()
+clf.fit(features_train,labels_train)
+print "Training time:" , round(time()-t0,3) , "s"
+t1 = time()
+pred = clf.predict(features_test)
+print "Predict time:", round(time()-t1,3) , "s"
+print clf.score(features_test,labels_test)

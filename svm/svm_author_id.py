261d885778b16eaa12f82837c956fbc3d800d722
Mayank Metha D <mayankmethad@gmail.com>
2017-02-08 15:57:19 +0530

    Update svm_author_id.py

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -27,4 +27,28 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 
-
+from sklearn import svm
+from sklearn.metrics import accuracy_score
+#m=svm.SVC(kernel='linear')
+#m = svm.SVC(kernel='rbf')
+#m = svm.SVC(kernel='rbf',C=10)
+#m = svm.SVC(kernel='rbf',C=100)
+#m = svm.SVC(kernel='rbf',C=1000)
+m = svm.SVC(kernel='rbf',C=10000)
+#features_train = features_train[:len(features_train)/100]
+#labels_train = labels_train[:len(labels_train)/100]
+t0 = time()
+m.fit(features_train,labels_train)
+print "Training time:" , round(time()-t0,3) , "s"
+t1 = time()
+pred=m.predict(features_test)
+print "Predict time:", round(time()-t1,3) , "s"
+print accuracy_score(labels_test,pred)
+#print pred[10]
+#print pred[26]
+#print pred[50]
+i = 0
+for a in pred:
+	if a==1:
+		i=i+1
+print i
